b3822cd2c37c6bf99c58d7c480b434e2ed0e48d2
MisakoArai <misakoarai888@gmail.com>
2021-09-05 17:15:52 +0900

    フィードバックの修正

--- MisakoooKadai3/CalculationViewController.swift
+++ MisakoooKadai3/CalculationViewController.swift
@@ -16,18 +16,30 @@ class CalculationViewController: UIViewController {
     @IBOutlet private weak var switch2: UISwitch!
     
     @IBAction private func didTapCalculationButton(_ sender: UIButton) {
-        var number1 = Int(numberTextField1.text!) ?? 0
-        var number2 = Int(numberTextField2.text!) ?? 0
+        // F：定数にする
+        let number1 = Int(numberTextField1.text!) ?? 0
+        let number2 = Int(numberTextField2.text!) ?? 0
         
-        if switch1.isOn {
-            number1 *= -1
-        }
+//        if switch1.isOn {
+//            number1 *= -1
+//        }
+//
+//        if switch2.isOn {
+//            number2 *= -1
+//        }
+//        enterdNumberLabel1.text = String(number1)
+//        enterdNumberLabel2.text = String(number2)
+//        calculationResultLabel.text = String(number1 + number2)
         
-        if switch2.isOn {
-            number2 *= -1
-        }
-        enterdNumberLabel1.text = String(number1)
-        enterdNumberLabel2.text = String(number2)
-        calculationResultLabel.text = String(number1 + number2)
+        // F：新たな意味合いの定数を都度作る
+        // 三項演算子、trueならマイナス
+        let signedNumber1 = switch1.isOn ? -number1 : number1
+        let signedNumber2 = switch2.isOn ? -number2 : number2
+        
+        // 定数を活用すると、このように何を表示しているのかが分かりやすくなります
+        // ラベルに値を入れる
+        enterdNumberLabel1.text = String(signedNumber1)
+        enterdNumberLabel2.text = String(signedNumber2)
+        calculationResultLabel.text = String(signedNumber1 + signedNumber2)
     }
 }
